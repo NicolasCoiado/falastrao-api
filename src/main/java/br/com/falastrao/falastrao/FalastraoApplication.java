653fd68d782b33fb8c05ba45fa0aa6229ac21224
NicolasCoiado <nicolasmcoiado@gmail.com>
2026-03-03 21:46:55 -0300

    chore: project structuring

---
+++ src/main/java/br/com/falastrao/falastrao/FalastraoApplication.java
@@ -0,0 +1,13 @@
+package br.com.falastrao.falastrao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalastraoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FalastraoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/falastrao/falastrao/FalastraoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.falastrao.falastrao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FalastraoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
