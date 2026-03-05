24463b380cdc97596bc28072ad3ee6f940cd58db
NicolasCoiado <nicolasmcoiado@gmail.com>
2026-03-05 19:55:09 -0300

    fix: correct typo in thumbnail_url column in reviews migration

--- src/main/resources/db/migration/V3__create_reviews_table.sql
+++ src/main/resources/db/migration/V3__create_reviews_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE reviews (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     user_id BIGINT NOT NULL,
     title VARCHAR(100) NOT NULL,
-    thumnail_url VARCHAR(255) NOT NULL,
+    thumbnail_url VARCHAR(255),
     content TEXT NOT NULL,
     published_at TIMESTAMPTZ NOT NULL,
     CONSTRAINT fk_reviews_users FOREIGN KEY (user_id) REFERENCES users(id)
