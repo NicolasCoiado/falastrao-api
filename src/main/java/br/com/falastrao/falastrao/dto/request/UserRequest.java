af079b81fe6942701c9f222745dfac54c20f1f88
NicolasCoiado <nicolasmcoiado@gmail.com>
2026-03-09 23:43:35 -0300

    refactor: update UserRequest validation annotations and constraints

--- src/main/java/br/com/falastrao/falastrao/dto/request/UserRequest.java
+++ src/main/java/br/com/falastrao/falastrao/dto/request/UserRequest.java
@@ -1,19 +1,19 @@
 package br.com.falastrao.falastrao.dto.request;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserRequest (
-        @NotEmpty(message = "Username is required.")
+        @NotBlank(message = "Username is required.")
         @Size(min = 1, max = 50)
         String username,
 
-        @NotEmpty(message = "Email is required.")
+        @NotBlank(message = "Email is required.")
         @Email(message = "Email should be valid.")
         String email,
 
-        @NotEmpty(message = "Password is required.")
+        @NotBlank(message = "Password is required.")
         @Size(min = 6, message = "Password must be at least 6 characters long.")
         String password
 ){}
