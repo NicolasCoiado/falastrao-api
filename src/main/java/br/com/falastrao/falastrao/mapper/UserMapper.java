e937e362bc2d18affbb2de554801dbc4bbd8e879
NicolasCoiado <nicolasmcoiado@gmail.com>
2026-03-09 23:44:57 -0300

    fix: correct UserMapper mappings between UserRequest, User and UserResponse

--- src/main/java/br/com/falastrao/falastrao/mapper/UserMapper.java
+++ src/main/java/br/com/falastrao/falastrao/mapper/UserMapper.java
@@ -4,9 +4,13 @@ import br.com.falastrao.falastrao.dto.request.UserRequest;
 import br.com.falastrao.falastrao.dto.response.UserResponse;
 import br.com.falastrao.falastrao.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    @Mapping(target = "username", source = "username")
+    @Mapping(target = "email", source = "email")
+    @Mapping(target = "password", source = "password")
     User toEntity(UserRequest request);
     UserResponse toResponse(User user);
 }
