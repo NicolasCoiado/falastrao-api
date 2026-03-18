473690611ac83c1f901078b916386cb9b4b8fb4c
NicolasCoiado <nicolasmcoiado@gmail.com>
2026-03-17 23:16:38 -0300

    feat: apply rate limiting to topic recommendation endpoint

--- src/main/java/br/com/falastrao/falastrao/config/WebConfig.java
+++ src/main/java/br/com/falastrao/falastrao/config/WebConfig.java
@@ -29,6 +29,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(rateLimitInterceptor)
-                .addPathPatterns("/topics/search", "/topics/ranked");
+                .addPathPatterns("/topics/search", "/topics/ranked", "/topics/suggest");
     }
 }
