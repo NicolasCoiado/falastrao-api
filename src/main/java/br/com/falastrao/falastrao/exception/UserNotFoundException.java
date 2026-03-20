7d4421d942749200027892d36409ee54bbad415a
NicolasCoiado <nicolasmcoiado@gmail.com>
2026-03-19 23:04:25 -0300

    feat: add UserNotFoundException for missing user scenarios

---
+++ src/main/java/br/com/falastrao/falastrao/exception/UserNotFoundException.java
@@ -0,0 +1,5 @@
+package br.com.falastrao.falastrao.exception;
+
+public class UserNotFoundException extends NotFoundException {
+    public UserNotFoundException(String message) { super(message); }
+}
