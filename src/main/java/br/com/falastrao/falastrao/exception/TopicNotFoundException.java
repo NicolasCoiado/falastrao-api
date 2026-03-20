f1b7bdf527eb28e85bbe2256fa6f2d3aff4e4dca
NicolasCoiado <nicolasmcoiado@gmail.com>
2026-03-19 23:04:21 -0300

    feat: add TopicNotFoundException for missing topic scenarios

---
+++ src/main/java/br/com/falastrao/falastrao/exception/TopicNotFoundException.java
@@ -0,0 +1,5 @@
+package br.com.falastrao.falastrao.exception;
+
+public class TopicNotFoundException extends NotFoundException {
+    public TopicNotFoundException(String message) { super(message); }
+}
