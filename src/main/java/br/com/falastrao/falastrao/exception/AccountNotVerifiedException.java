cac8ba51eb9ec1b6cf425e389b77993ba0e43194
NicolasCoiado <nicolasmcoiado@gmail.com>
2026-03-19 23:03:59 -0300

    feat: add AccountNotVerifiedException for unverified account access

---
+++ src/main/java/br/com/falastrao/falastrao/exception/AccountNotVerifiedException.java
@@ -0,0 +1,5 @@
+package br.com.falastrao.falastrao.exception;
+
+public class AccountNotVerifiedException extends AccountException {
+    public AccountNotVerifiedException(String message) { super(message); }
+}
