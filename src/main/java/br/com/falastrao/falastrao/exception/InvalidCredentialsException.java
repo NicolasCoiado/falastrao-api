37ed8794522490da9ca7fc3b52a309713d28ef6c
NicolasCoiado <nicolasmcoiado@gmail.com>
2026-03-19 23:04:13 -0300

    feat: add InvalidCredentialsException for authentication failures

---
+++ src/main/java/br/com/falastrao/falastrao/exception/InvalidCredentialsException.java
@@ -0,0 +1,5 @@
+package br.com.falastrao.falastrao.exception;
+
+public class InvalidCredentialsException extends AuthException {
+    public InvalidCredentialsException(String message) { super(message); }
+}
