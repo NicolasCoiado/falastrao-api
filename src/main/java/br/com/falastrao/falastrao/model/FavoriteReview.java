4262b139605fddd43ac3b495d84b34c774dd3d58
NicolasCoiado <nicolascoiado03@hotmail.com>
2026-05-20 19:55:08 -0300

    feat: add favorite review entity

---
+++ src/main/java/br/com/falastrao/falastrao/model/FavoriteReview.java
@@ -0,0 +1,35 @@
+package br.com.falastrao.falastrao.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name = "user_favorite_reviews")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class FavoriteReview {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id", nullable = false)
+    private Review review;
+
+    @CreationTimestamp
+    @Column(name = "favorited_at", nullable = false, updatable = false)
+    private OffsetDateTime favoritedAt;
+}
